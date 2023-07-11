12fb2f970b9ae8b179de950fbd21af848b66f375
Dennis Schmitt <peppie23@gmail.com>
2023-07-11 10:20:53 +0200

    Added different conversion factors

--- FS22_CutterConverter/src/cutterconverter.lua
+++ FS22_CutterConverter/src/cutterconverter.lua
@@ -1,33 +1,49 @@
 --
 -- Main
 --
--- Main class for initialize the cutter converter.
+-- Main class for initialize the WinterX converters.
 --
 -- Copyright (c) Peppie84, 2023
 --
 local cutterConverterData = {
     WINTERWHEAT_CONVERTER = {
         FROM_FRUIT = 'WINTER_WHEAT',
-        TO_FRUIT = 'WHEAT'
+        TO_FRUIT = 'WHEAT',
+        CONVERSION = 1.2
     },
     WINTERBARLEY_CONVERTER = {
         FROM_FRUIT = 'WINTER_BARLEY',
-        TO_FRUIT = 'BARLEY'
+        TO_FRUIT = 'BARLEY',
+        CONVERSION = 1.4
     }
+}
+
+local cutterConverterConstants = {
+    CONVERTER_IS_BASE_TYPE = false
 }
 
 ---Mission00 is loading
 ---@param mission table (Mission00)
 local function preload(mission)
-    local conversionFactor = 1
     local windrowConversionFactor = 0
 
     for cutterConverterName, cutterConverterDesc in pairs(cutterConverterData) do
         -- Convert FROM_FRUIT to TO_FRUIT
         local fromFruitTypeDesc   = g_fruitTypeManager:getFruitTypeByName(cutterConverterDesc.FROM_FRUIT)
         local toFruitFillTypeDesc = g_fillTypeManager:getFillTypeByName(cutterConverterDesc.TO_FRUIT)
-        local converterIndex      = g_fruitTypeManager:addFruitTypeConverter(cutterConverterName, true)
-        g_fruitTypeManager:addFruitTypeConversion(converterIndex, fromFruitTypeDesc.index, toFruitFillTypeDesc.index, conversionFactor, windrowConversionFactor)
+
+        local converterIndex = g_fruitTypeManager:addFruitTypeConverter(
+            cutterConverterName,
+            cutterConverterConstants.CONVERTER_IS_BASE_TYPE
+        )
+
+        g_fruitTypeManager:addFruitTypeConversion(
+            converterIndex,
+            fromFruitTypeDesc.index,
+            toFruitFillTypeDesc.index,
+            cutterConverterDesc.CONVERSION,
+            windrowConversionFactor
+        )
     end
 end
 
